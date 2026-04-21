0d3612feaf29f25fb9fdb391514f5d506252a9cd
Sachinsingh6677 <sachinsingh_aiml24@acem.edu.in>
2026-04-22 00:55:34 +0530

    Updated console logs

--- backend/server.js
+++ backend/server.js
@@ -90,6 +90,10 @@ app.use((req, res) => {
 
 // Error handler middleware (must be last)
 app.use(errorHandler);
+app.use((req, res, next) => {
+  console.log("👉", req.method, req.url);
+  next();
+});
 
 // Start server
 const PORT = process.env.PORT || 5000;
